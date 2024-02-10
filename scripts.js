877e378ccd1979f350618fce5fa7e0d313595926
ka830x <kasparskoda@gmail.com>
2024-02-10 09:34:53 +0100

    js überarbeitet und funktioniert jetzt, gallerie zwar falsch formatiert aber zum testen pushe ich

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
-                  function loadImages() {
-            const gallery = document.getElementById('image-gallery');
-            const folderPath = './Images/ImageGallery';
-        
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', folderPath, true);
-        
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    const parser = new DOMParser();
-                    const htmlDoc = parser.parseFromString(xhr.responseText, 'text/html');
-                    const images = Array.from(htmlDoc.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"], a[href$=".gif"]'));
-        
-                    images.forEach(image => {
-                        const imageUrl = folderPath + '/' + image.getAttribute('href');
-                        const imgElement = document.createElement('img');
-                        imgElement.src = imageUrl;
-                        gallery.appendChild(imgElement);
-                    });
-                } else {
-                    console.error('Failed to load images. Status:', xhr.status);
-                }
-            };
-        
-            xhr.send();
+function loadImages() {
+    const gallery = document.getElementById('image-gallery');
+    const folderPath = './Images/ImageGallery';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', folderPath, true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const parser = new DOMParser();
+            const htmlDoc = parser.parseFromString(xhr.responseText, 'text/html');
+            const images = Array.from(htmlDoc.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"], a[href$=".gif"]'));
+
+            images.forEach(image => {
+                const imgElement = document.createElement('img');
+                imgElement.src = image.getAttribute("href");
+                gallery.appendChild(imgElement);
+            });
+        } else {
+            console.error('Failed to load images. Status:', xhr.status);
         }
-        
-        window.onload = loadImages;
+    };
+
+    xhr.send();
+}
+
+window.onload = loadImages;
