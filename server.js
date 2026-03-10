74c11294e65818393677c695a293693a37302c30
vxnsin <vxnsin@icloud.com>
2026-03-10 15:48:41 +0100

    fix: remove file size limit from multer configuration

--- server.js
+++ server.js
@@ -116,7 +116,6 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) cb(null, true);
     else cb(new Error("Nur Bilder erlaubt!"), false);
